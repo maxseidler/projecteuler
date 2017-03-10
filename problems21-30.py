a4db7a18bc5ff7cf72828906379fd8569a912027
Maximilian Seidler <maxseidler@gmx.net>
2017-03-10 19:15:54 +0100

    Added problems 22 and 23

--- problems21-30.py
+++ problems21-30.py
@@ -17,4 +17,31 @@ def problem21():
     print(amicable_sum)
 
 
-problem21()
+def problem22():
+    """
+    Names scores
+    """
+    names = open('files/problem22_file.txt').read()
+    sorted_names = [name.replace('"', '') for name in sorted(names.split(','))]
+    name_score = 0
+    for i in range(0, len(sorted_names)):
+        name_score += sum(ord(char)-64 for char in sorted_names[i]) * (i+1)
+    print(name_score)
+
+
+def sum_of_two_abundant_numbers(n, abundant_numbers):
+    for abund_n in abundant_numbers:
+        if n - abund_n in abundant_numbers:
+            return True
+    return False
+
+
+def problem23():
+    """
+    Non-abundant sums
+    """
+    abundant_numbers = set(n for n in range(0, 28124) if sum(divisors(n)) > n)
+    print(sum(n for n in range(1, 28124) if not sum_of_two_abundant_numbers(n, abundant_numbers)))
+
+
+problem23()
